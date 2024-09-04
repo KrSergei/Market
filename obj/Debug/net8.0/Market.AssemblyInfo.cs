39ed452003a31f4d0262910c2f9401823554c183
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-09-04 21:54:55 +0300

    1

--- obj/Debug/net8.0/Market.AssemblyInfo.cs
+++ obj/Debug/net8.0/Market.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Market")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26739803687cf06477bd525196ad3e4749924396")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847664fc333321042ac08df17a8ddec0897c5e60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Market")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Market")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
